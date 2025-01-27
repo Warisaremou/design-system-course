d3feebb4eb0c68024210227342bce33a503abe60
Badirou Imane <abenibadirou99@gmail.com>
2025-01-27 09:53:48 +0100

    Setup project

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Imane from './textarea'
+
+function App() {
+  
+
+  return (
+    <>
+     <Imane />
+    </>
+  )
+}
+
+export default App
---
+++ src/textarea.jsx
@@ -0,0 +1,12 @@
+function Imane () {
+  return (
+    
+    <div>
+      <textarea className="w-80 h-800 bg-white-200 border border-black rounded-xxl"></textarea>
+    </div>
+  );
+}
+
+
+
+export default Imane;
