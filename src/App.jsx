806f7a8f801c2a67c912555f72445a15560ae837
Waris Aremou <warisaremou2003@gmail.com>
2025-02-02 15:35:20 +0100

    Fix: Design System

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Textarea from "./components/Ui/textarea";
 
 function App() {
 	return (
-		<div>
-			<div className="p-5 border w-fit space-y-5 rounded-md border-grey-200">
-				<div>
+		<div className="grid grid-cols-3 p-5 gap-5 [&>div]:space-y-5">
+			<div>
+				<div className="flex flex-col gap-3">
 					<h1>TYPOGRAPHY</h1>
 					<div>
 						<h1>Heading 1</h1>
@@ -12,19 +12,83 @@ function App() {
 						<p className="body">Body Regular</p>
 						<p className="body-medium">Body Medium</p>
 						<p className="body-semibold">Body Semibold</p>
-						<span className="caption text-state-error">Caption</span>
+						<span className="caption">Caption</span>
 					</div>
 				</div>
-				<div className="size-36 bg-linear" />
+				<div className="flex flex-col gap-3">
+					<h1>COLORS</h1>
+					<div>
+						<h2>Gradient</h2>
+						<div className="size-24 bg-linear" />
+					</div>
+					<div>
+						<h2>Primary</h2>
+						<div className="flex gap-1">
+							<div>
+								<p className="body-medium">primary-100</p>
+								<div className="size-24 bg-primary-100" />
+							</div>
+							<div>
+								<p className="body-medium">primary-200</p>
+								<div className="size-24 bg-primary-200" />
+							</div>
+							<div>
+								<p className="body-medium">primary-300</p>
+								<div className="size-24 bg-primary-300" />
+							</div>
+						</div>
+					</div>
+					<div>
+						<h2>Grey</h2>
+						<div className="flex gap-1">
+							<div>
+								<p className="body-medium">grey-100</p>
+								<div className="size-24 bg-grey-100" />
+							</div>
+							<div>
+								<p className="body-medium">grey-200</p>
+								<div className="size-24 bg-grey-200" />
+							</div>
+							<div>
+								<p className="body-medium">grey-300</p>
+								<div className="size-24 bg-grey-300" />
+							</div>
+							<div>
+								<p className="body-medium">grey-400</p>
+								<div className="size-24 bg-grey-400" />
+							</div>
+						</div>
+					</div>
+					<div>
+						<h2>State</h2>
+						<div className="flex gap-1">
+							<div>
+								<p className="body-medium">state-error</p>
+								<div className="size-24 bg-state-error" />
+							</div>
+							<div>
+								<p className="body-medium">state-success</p>
+								<div className="size-24 bg-state-success" />
+							</div>
+							<div>
+								<p className="body-medium">state-warning</p>
+								<div className="size-24 bg-state-warning" />
+							</div>
+						</div>
+					</div>
+				</div>
 			</div>
-			<div className="p-5 m-2 border w-fit space-y-5 rounded-md border-gray-200">
-				<Textarea placeholder="Textarea" borderColor="border-gray-300" />
-				<Textarea placeholder="Textarea" borderColor="border-black" />
-				<Textarea placeholder="Textarea" borderColor="border-purple-500" />
-				<Textarea placeholder="Textarea" borderColor="border-purple-700" />
-				<Textarea placeholder="Textarea" borderColor="border-red-500" />
-				<Textarea placeholder="Textarea" borderColor="border-green-500" />
-				<Textarea placeholder="Textarea" borderColor="border-green-700" />
+			<div>
+				<h1>Textarea</h1>
+				<div className="space-y-5">
+					<Textarea placeholder="Textarea" borderColor="border-gray-300" />
+					<Textarea placeholder="Textarea" borderColor="border-black" />
+					<Textarea placeholder="Textarea" borderColor="border-purple-500" />
+					<Textarea placeholder="Textarea" borderColor="border-purple-700" />
+					<Textarea placeholder="Textarea" borderColor="border-red-500" />
+					<Textarea placeholder="Textarea" borderColor="border-green-500" />
+					<Textarea placeholder="Textarea" borderColor="border-green-700" />
+				</div>
 			</div>
 		</div>
 	);
